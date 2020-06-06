0851a8f0647115d8653e58b1fcc5d815a20d19b7
warren <varcolac01@gmail.com>
2020-06-06 23:37:56 +0100

    switch to inline buttons

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
+from telegram import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 
 buttons = ["FASS", "Business & Accountancy", "Computing", "Dentistry", "Engineering", "Law",
            "Medicine", "Science", "Halls/Residential Colleges", "Sports Facilities", "Others"]
+comp = ["SOC Announcements", "CS1101S Attendance Bot", "TeleSource"]
 
 def makeButtons(array):
     number = len(array)
     isEven = (number % 2 == 0)
-    buttonList = [KeyboardButton(s) for s in array]
+    i = 0
+    buttonList = []
+    while i < number:
+        buttonList.append(InlineKeyboardButton(array[i], callback_data = array[i]))
+        i += 1
     newList = []
     for i in range(0, number // 2):
         newArray = []
@@ -21,27 +26,22 @@ def makeButtons(array):
 
 def start(update, context):
     button_list = makeButtons(buttons)
-    keyboard = ReplyKeyboardMarkup(keyboard=button_list, resize_keyboard=True)
+    keyboard = InlineKeyboardMarkup(button_list)
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome! Here you can find " + 
     "many useful bots and announcement channels that you may find useful during your studies in " +
     "NUS. Please select from the custom keyboard the faculty you are interested in finding bots" +
-    "/channels", reply_markup=keyboard)
+    "/channels.", reply_markup=keyboard)
 
-def regMes(update, context):
-    content = update.message.text
-    finished = False
-    for cat in buttons:
-        if content == cat:
-            reply(update.effective_chat.id, context)
-            finished = True
-            break
-        else:
-            continue
-    if not finished:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, unsupported text.")
-
-def reply(chatID, context):
-    context.bot.send_message(chat_id=chatID, text="Work in Progress.")
+def button(update, context):
+    query = update.callback_query
+    query.answer()
+    reply = query.data
+    if reply == "Computing":
+        buttons = makeButtons(comp)
+        keyboard = InlineKeyboardMarkup(buttons)
+        query.edit_message_text(text = "abc", reply_markup=keyboard)
+    else:
+        query.edit_message_text(text="Selected option: {}".format(query.data))
 
 def main():
     #Remember to remove token before commit
@@ -50,7 +50,7 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), regMes))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
     updater.start_polling()
     updater.idle()
